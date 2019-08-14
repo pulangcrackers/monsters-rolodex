d9dc31ef09aeed2d5abdea1d9a17151c29a8a982
pulangcrackers <bmariepaula121@gmail.com>
2019-08-14 14:38:37 +0800

    Updates

---
+++ precache-manifest.d8c6ac028b80cc10aeecfa07ad4bf134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc9aa94dd2cc87a548749d0447b80a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2c44739372eedb24ea11",
+    "url": "/monsters-rolodex/static/css/main.84e088b8.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "2c44739372eedb24ea11",
+    "url": "/monsters-rolodex/static/js/main.8ce93425.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
